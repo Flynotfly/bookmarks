c13b27d218012c76aa4fcc4395ca5d2a505da174
Flynotfly <Alteria2004@gmail.com>
2024-07-28 19:49:18 +0400

    add urls for password reset

--- account/urls.py
+++ account/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
 
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset-done/', auth_views.PasswordResetDoneView(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView(), name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView(), name='password_reset_complete'),
 ]
