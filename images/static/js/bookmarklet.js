cceaea57bc6b5198aa03b66f5c0d80b0e9caccc6
Flynotfly <Alteria2004@gmail.com>
2024-07-31 09:15:24 +0400

    create bookmarklet.js

---
+++ images/static/js/bookmarklet.js
@@ -0,0 +1,12 @@
+const siteUrl = 'https://mysite.com:8000/';
+const styleUrl = siteUrl + 'static/css/bookmarklet.css';
+const minWidth = 250;
+const minHeight = 250;
+
+// load CSS
+var head = document.getElementsByTagName('head')[0];
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = styleUrl + '?=' + Math.floor(Math.random() * 9999999999999999);
+head.appendChild(link);
