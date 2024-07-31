4c8c3e85da287e5337577cd500294016315c1fa5
Flynotfly <Alteria2004@gmail.com>
2024-07-31 07:59:10 +0400

    add save method to the ImageCreateForm

--- images/forms.py
+++ images/forms.py
@@ -1,4 +1,7 @@
+import requests
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from images.models import Image
 
 
@@ -17,3 +20,15 @@ class ImageCreateForm(forms.ModelForm):
         if extensions not in valid_extensions:
             raise forms.ValidationError('This given URL doen not match valid image extensions.')
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data('url')
+        name = slugify(image.title)
+        exetsion = image_url.rsplir('.', 1)[1].lower()
+        image_name = f'{name}.{exetsion}'
+        responce = requests.get(image_url)
+        image.image.save(image_name, ContentFile(responce.content), save=False)
+        if commit:
+            image.save()
+        return image
