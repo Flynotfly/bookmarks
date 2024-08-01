17d9cf0ce35c87912dbd9f82898325924e0c5a17
Flynotfly <Alteria2004@gmail.com>
2024-08-01 08:35:19 +0400

    debug bookmarklet.js

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -1,4 +1,4 @@
-const siteUrl = 'https://mysite.com:8000/';
+const siteUrl = '//127.0.0.1:8000/';
 const styleUrl = siteUrl + 'static/css/bookmarklet.css';
 const minWidth = 250;
 const minHeight = 250;
@@ -8,10 +8,10 @@ var head = document.getElementsByTagName('head')[0];
 var link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
-link.href = styleUrl + '?=' + Math.floor(Math.random() * 9999999999999999);
+link.href = styleUrl + '?r=' + Math.floor(Math.random() * 9999999999999999);
 head.appendChild(link);
 
-// load HTNL
+// load HTML
 var body = document.getElementsByTagName('body')[0];
 boxHtml = `
     <div id="bookmarklet">
@@ -21,7 +21,7 @@ boxHtml = `
     </div>`;
 body.innerHTML += boxHtml;
 
-function bookmarletLaunch() {
+function bookmarkletLaunch() {
     bookmarklet = document.getElementById('bookmarklet');
     var imagesFound = bookmarklet.querySelector('.images');
 
@@ -35,14 +35,23 @@ function bookmarletLaunch() {
             bookmarklet.style.display = 'none'
         });
 
-    images = document.querySelectorAll('img[scr$=".jpg"], img[src$=".jprg"], imp[src$="png"]');
+    images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
     images.forEach(image => {
         if (image.naturalWidth >= minWidth && image.naturalHeight >= minHeight) {
             var imageFound = document.createElement('img');
             imageFound.src = image.src;
-            imageFound.append(imageFound);
+            imagesFound.append(imageFound);
         }
     })
+    //
+    // imagesFound.querySelectorAll('img').forEach(image => {
+    //     image.addEventListener('click', function (event){
+    //         imageSelected = event.target;
+    //         bookmarklet.style.display = 'none';
+    //         window.open(siteUrl + 'images/create/?url=' + encodeURIComponent(imageSelected.src) + '&title='
+    //                     + encodeURIComponent(document.title), '_blank');
+    //     })
+    // })
 }
 
-bookmarletLaunch()
+bookmarkletLaunch()
--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -1,7 +1,7 @@
 (function () {
     if (!window.bookmarklet) {
         bookmarklet_js = document.body.appendChild(document.createElement('script'));
-        bookmarklet_js.src = 'https://mysite:8000/stasic/jss/bookmarklet.js?r=' +
+        bookmarklet_js.src = '//127.0.0.1:8000/stasic/jss/bookmarklet.js?r=' +
             Math.floor(Math.random() * 9999999999999999);
         window.bookmarklet = true;
     } else {
