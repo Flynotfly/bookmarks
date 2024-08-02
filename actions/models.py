8710950c7e026415e9c0c7b79dafafe08d86e724
Flynotfly <Alteria2004@gmail.com>
2024-08-02 15:44:32 +0400

    create Action model

--- actions/models.py
+++ actions/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Action(models.Model):
+    user = models.ForeignKey(
+        settings.USER_AUTH_MODEL,
+        related_name='actions',
+        on_delete=models.CASCADE()
+    )
+    varb = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+        ordering=['-created']
